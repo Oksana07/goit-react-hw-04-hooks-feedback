b916eb81112220e8251f83be58706885d72e6549
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-10-25 17:43:15 +0300

    Home work

---
+++ src/components/App/App.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import FeedbackOptions from "../FeedbackOptions";
+import Statistics from "../Statistics";
+import Section from "../Section";
+import Container from "../Container";
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = (e) => {
+    switch (e) {
+      case "good":
+        setGood((prev) => prev + 1);
+        break;
+      case "neutral":
+        setNeutral((prev) => prev + 1);
+        break;
+      case "bad":
+        setBad((prev) => prev + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const allReviews = countTotalFeedback();
+    const positivePersent = good;
+    return good ? Math.ceil((positivePersent / allReviews) * 100) : 0;
+  };
+
+  return (
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={["good", "neutral", "bad"]}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      </Section>
+    </Container>
+  );
+}
+
+export default App;
